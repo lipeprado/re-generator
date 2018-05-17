3e92e0369417ee6032b66964c49ad6c8204ac0ec
Filipe Prado <fi-lipe2010@hotmail.com>
2018-05-17 15:14:51 -0300

    New Version

--- src/command/commander.js
+++ src/command/commander.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { fullRedux, stateLess } = require('../content/contents');
 
 const createDir = (dir) => {
-  const road = path.join(__dirname, `../${dir}`);
+  const road = path.join(__dirname, `../../../${dir}`);
   if (fs.existsSync(road)) return road;
   fs.mkdirSync(path.join(road));
   return road;
