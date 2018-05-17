2db7f7596d9650be25ff4d5ea75abb6ba9d07527
Filipe Prado <fi-lipe2010@hotmail.com>
2018-05-17 15:39:25 -0300

    Removing Versions

---
+++ src/Container2/HeaderContainer.jsx
@@ -0,0 +1,17 @@
+
+    import React, { Component } from 'react';
+    import PropTypes from 'prop-types';
+    
+    class Header extends Component {
+        render() {
+            return (
+                <div>Component Header</div>
+            );
+        }
+    }
+    
+    Header.propTypes = {
+    
+    };
+    
+    export default Header;
